b9521e0d6bd6a5568a852845254f949eb76e4573
yuqiaobinbin <62179758+yuqiaobinbin@users.noreply.github.com>
2020-12-21 22:53:59 +0800

    美化界面

--- app/src/main/java/com/example/mylocker_ver1/MainActivity.java
+++ app/src/main/java/com/example/mylocker_ver1/MainActivity.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
@@ -16,18 +20,22 @@ import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mylocker_ver1.algorithm.SaveImage;
 import com.example.mylocker_ver1.SaveImageActivity;
+import com.lihang.smartloadview.SmartLoadingView;
+import com.stephentuso.welcome.WelcomeHelper;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.osgi.OpenCVNativeLoader;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox lockSwitcher;
@@ -36,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
     private SaveImageActivity SaveCoord = null;
     Button open_lock;
     boolean isPush = false;
+    ImageView imageView;
+    TextView textView;
+    Time t;
+    SmartLoadingView loadingView;
+    WelcomeHelper welcomeScreen;
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -50,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
         // 申请覆盖在应用程序上层的权限，否则会出现闪退
         if (Build.VERSION.SDK_INT >= 23) {
             if (!Settings.canDrawOverlays(this)) {
@@ -58,12 +74,53 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         }
-
         new OpenCVNativeLoader().init();
 
         final SettingPresenter settingPresenter = new SettingPresenter(this);
         // 设置锁屏的界面
-        setContentView(R.layout.activity_main);
+        t = new Time(); // or Time t=new Time("GMT+8"); 加上Time Zone资料
+        t.setToNow(); // 取得系统时间。
+        int hour = t.hour;
+
+
+        loadingView = findViewById(R.id.bt_open_lock);
+        loadingView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPush = !isPush;
+                if (loadingView.isFollow()) {
+                    loadingView.reset();
+                } else {
+                    loadingView.start();
+                    new Handler().postDelayed(new  Runnable(){
+                        public  void  run() {
+                            loadingView.onSuccess(new SmartLoadingView.AnimationOKListener() {
+                                @Override
+                                public void animationOKFinish() {
+                                }
+                            });
+                        }
+                    },  2000 );
+
+                }
+                settingPresenter.setLockStatus(isPush);
+            }
+        });
+
+        Log.d("timetimetime", String.valueOf(hour));
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        if (hour >= 6 && hour < 12) {
+            imageView.setImageResource(R.drawable.good_morning_img);
+            textView.setText("早上");
+        } else if (hour >= 12 && hour < 18) {
+            imageView.setImageResource(R.drawable.good_morning_img);
+            textView.setText("下午");
+        } else {
+            imageView.setImageResource(R.drawable.good_night_img);
+            textView.setText("晚上");
+        }
+
 //        // 服务开关的按钮
 //        lockSwitcher = findViewById(R.id.lock_switcher);
 //        lockSwitcher.setChecked(LockService.running);
@@ -82,51 +139,6 @@ public class MainActivity extends AppCompatActivity {
 //                        isChecked ? "开启" : "关闭"));
 //            }
 //        });
-
-        open_lock = findViewById(R.id.bt_open_lock);
-        open_lock.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                RotateAnimation anim = new RotateAnimation(0.0f, 360.0f , Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, .5f);
-                Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotate);
-                anim.setInterpolator(new LinearInterpolator());
-                anim.setDuration(1000);
-                open_lock.setBackgroundResource(R.drawable.button_circle_changing);
-                open_lock.setEnabled(false);
-                open_lock.setText("");
-                open_lock.setAnimation(anim);
-                open_lock.startAnimation(anim);
-//                open_lock.clearAnimation();
-//                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotate);
-//                open_lock.setAnimation(animation);
-//                animation.start();
-                isPush = !isPush;
-                anim.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        if(isPush){
-                            open_lock.setText("已开启");
-                            open_lock.setEnabled(true);
-                            open_lock.setBackgroundResource(R.drawable.button_circle_open);
-                        }else{
-                            open_lock.setText("开启锁屏");
-                            open_lock.setEnabled(true);
-                            open_lock.setBackgroundResource(R.drawable.button_circle);
-                        }
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-                    }
-                });
-
-                settingPresenter.setLockStatus(isPush);
-            }
-        });
 
         Button set_bg = findViewById(R.id.set_lock_bg);
         set_bg.setOnClickListener((new View.OnClickListener() {
---
+++ app/src/main/java/com/example/mylocker_ver1/WelcomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.mylocker_ver1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.mylocker_ver1.Util.SharedPreferencesUtil;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*首先启动该Activity，并判断是否是第一次启动,注意，需要添加默认值,
+        * 如果是第一次启动，则先进入功能引导页*/
+        boolean isFirstOpen = SharedPreferencesUtil.getBoolean(this, SharedPreferencesUtil.FIRST_OPEN, true);
+        if(isFirstOpen){
+            Intent intent = new Intent(this, WelcomeGuideActivity.class);
+            startActivity(intent);
+            /*注意，需要使用finish将该activity进行销毁，否则，在按下手机返回键时，会返回至启动页*/
+            finish();
+            return;
+        }
+        /*如果不是第一次启动app，则启动页*/
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /*2秒后进入主页*/
+                enterHomeActivity();
+            }
+        },1000);
+    }
+
+    private void enterHomeActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
